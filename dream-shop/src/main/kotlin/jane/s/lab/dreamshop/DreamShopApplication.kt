f50f857fc2284f8247d9c043097f97ab2794029c
janeljs <jisunlim818@gmail.com>
2022-04-29 10:36:53 +0900

    프로젝트 설정

---
+++ dream-shop/src/main/kotlin/jane/s/lab/dreamshop/DreamShopApplication.kt
@@ -0,0 +1,11 @@
+package jane.s.lab.dreamshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DreamShopApplication
+
+fun main(args: Array<String>) {
+    runApplication<DreamShopApplication>(*args)
+}
---
+++ dream-shop/src/test/kotlin/jane/s/lab/dreamshop/DreamShopApplicationTests.kt
@@ -0,0 +1,13 @@
+package jane.s.lab.dreamshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DreamShopApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
